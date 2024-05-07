9475d137d2a4bf1d5f110c6535039fa53aa2714d
mapleZhu <151593811+mapleZhu@users.noreply.github.com>
2024-05-07 23:15:05 +0800

    Add files via upload

---
+++ Project/Lab9.srcs/sources_1/new/switches.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/05/07 22:23:23
+// Design Name: 
+// Module Name: switches
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module switches(
+
+    );
+endmodule
