31893c7a9435c8f4581339d975ec33f249c1f30f
mapleZhu <295909576@qq.com>
2024-05-09 14:04:24 +0800

    add clock_divider

--- Project/Lab9.srcs/sources_1/new/CPU.v
+++ Project/Lab9.srcs/sources_1/new/CPU.v
@@ -1,8 +1,12 @@
-module CPU(fpga_rst,fpga_clk,switch16,led16,start_pg,rx,tx,digit_led7);
+module CPU(fpga_rst,fpga_clk,switch16,button5,led16,tub_sel1,tub_sel2,tub_control1,tub_control2,start_pg,rx,tx,digit_led7);
     input fpga_rst,fpga_clk,start_pg,rx;
     input[15:0] switch16;
     input [4:0]button5;
     output[15:0] led16;
+    output [3:0] tub_sel1;
+    output [3:0] tub_sel2;
+    output [7:0] tub_control1;
+    output [7:0] tub_control2;
     output tx;
     output [31:0] digit_led7;
     wire [31:0] PC;
@@ -17,6 +21,8 @@ module CPU(fpga_rst,fpga_clk,switch16,led16,start_pg,rx,tx,digit_led7);
     wire [14:0] upg_adr_o;
     wire [31:0] upg_dat_o;
     wire spg_bufg;
+    wire [15:0]switchData;
+    wire tube_clk;
     BUFG U1(.I(start_pg), .O(spg_bufg));     // de-twitter
     // Generate UART Programmer reset signal
     reg upg_rst;
@@ -27,6 +33,7 @@ module CPU(fpga_rst,fpga_clk,switch16,led16,start_pg,rx,tx,digit_led7);
     //used for other modules which don't relate to UART
     wire rst;      
     assign rst = fpga_rst | !upg_rst;
+    clk_divider tube_clock (upg_clk,tube_clk);
     cpuclk uclk( .clk_in1(fpga_clk), .clk_out1(cpuclk), .clk_out2(upg_clk));
     IFetch uif(cpuclk,Branch,zero,imm,PC);
     programrom(
@@ -56,7 +63,7 @@ module CPU(fpga_rst,fpga_clk,switch16,led16,start_pg,rx,tx,digit_led7);
         .upg_done_i(rx));
     MemOrIO mio(MemRead, MemWrite, IORead, IOWrite,ram_dat, ioread_data, Wdata, Rdata1, write_data, ledcs, switchcs);
     ioread uior(rst,IORead,switchcs,switch16,ioread_data);
-    leds uled(rst,cpuclk,IOWrite,ledcs,ALUResult[1:0],write_data,led16,digit_led7);
+    leds uled(rst,cpuclk,IOWrite,ledcs,ALUResult[1:0],write_data,led16);
     seven_segment_tube tube_tb(1'b1,32'h12345678,tube_clk,tub_sel1,tub_sel2,tub_control1,tub_control2);//test
-    switches uswitch(cpuclk,rst,switchCtrl,button5,ALUResult[1:0],switches,switchData);
+    switches uswitch(cpuclk,rst,IORead,button5,ALUResult[1:0],switch16,switchData);
 endmodule
--- Project/Lab9.srcs/sources_1/new/leds.v
+++ Project/Lab9.srcs/sources_1/new/leds.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 
-module leds (ledrst,led_clk,ledwrite,ledcs,ledaddr,ledwdata,ledout,digit_led7);
+module leds (ledrst,led_clk,ledwrite,ledcs,ledaddr,ledwdata,ledout);
     input			ledrst;		// reset, active high (��λ�ź�,�ߵ�ƽ��Ч)
     input            led_clk;    // clk for led (ʱ���ź�)
     input            ledwrite;    // led write enable, active high (д�ź�,�ߵ�ƽ��Ч)
@@ -9,10 +9,8 @@ module leds (ledrst,led_clk,ledwrite,ledcs,ledaddr,ledwdata,ledout,digit_led7);
     input    [2:0]    ledaddr;    // 2'b00 means updata the low 16bits of ledout, 2'b10 means updata the high 8 bits of ledout
     input    [31:0]    ledwdata;    // the data (from register/memorio)  waiting for to be writen to the leds of the board
     output    [15:0]    ledout;        // the data writen to the leds  of the board
-    output    [31:0]    digit_led7;
   
     reg [15:0] ledout;
-    reg [31:0] digit_led7;
     
     always @ (posedge led_clk or posedge ledrst) begin
         if (ledrst)
@@ -23,8 +21,6 @@ module leds (ledrst,led_clk,ledwrite,ledcs,ledaddr,ledwdata,ledout,digit_led7);
 				ledout[15:0] <= {ledwdata[7:0],ledout[7:0]};
 			else if (ledaddr == 3'b010 )
 				ledout[15:0] <= {ledout[15:8],ledwdata[7:0]};
-			else if (ledaddr == 3'b100 )
-			    digit_led7[31:0] <= ledwdata[31:0];
 			else
 				ledout <= ledout;
         end else begin
