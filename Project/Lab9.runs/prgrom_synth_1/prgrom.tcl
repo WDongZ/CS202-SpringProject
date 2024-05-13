fe4b9474431b5a4893254e708b94135400f764b7
WDongZ <505314385@qq.com>
2024-05-13 23:29:06 +0800

    upd

--- Project/Lab9.runs/prgrom_synth_1/prgrom.tcl
+++ Project/Lab9.runs/prgrom_synth_1/prgrom.tcl
@@ -16,6 +16,9 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_param synth.incrementalSynthesisCache C:/Users/ASUS/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-5852-LAPTOP-I606K2C4/incrSyn
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 set_param project.vivado.isBlockSynthRun true
 set_msg_config -msgmgr_mode ooc_run
 create_project -in_memory -part xc7a35tcsg324-1
@@ -24,16 +27,16 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.cache/wt [current_project]
-set_property parent.project_path C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.xpr [current_project]
+set_property webtalk.parent_dir G:/Study2024S/CS202-SpringProject/Project/Lab9.cache/wt [current_project]
+set_property parent.project_path G:/Study2024S/CS202-SpringProject/Project/Lab9.xpr [current_project]
 set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_repo_paths c:/Users/86153/Desktop/cpu2/CS202ComputerOrganization/SEU_CSE_507_user_uart_bmpg_1.3 [current_project]
-set_property ip_output_repo c:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.cache/ip [current_project]
+set_property ip_repo_paths g:/Study2024S/CS202ComputerOrganization/SEU_CSE_507_user_uart_bmpg_1.3 [current_project]
+set_property ip_output_repo g:/Study2024S/CS202-SpringProject/Project/Lab9.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_ip -quiet C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom.xci
-set_property used_in_implementation false [get_files -all c:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_ooc.xdc]
+read_ip -quiet G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom.xci
+set_property used_in_implementation false [get_files -all g:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_ooc.xdc]
 
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -46,7 +49,7 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
 read_xdc dont_touch.xdc
 set_property used_in_implementation false [get_files dont_touch.xdc]
 
-set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1 -new_name prgrom -ip [get_ips prgrom]]
+set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1 -new_name prgrom -ip [get_ips prgrom]]
 
 if { $cached_ip eq {} } {
 
@@ -85,32 +88,32 @@ write_checkpoint -force -noxdef prgrom.dcp
 create_report "prgrom_synth_1_synth_report_utilization_0" "report_utilization -file prgrom_utilization_synth.rpt -pb prgrom_utilization_synth.pb"
 
 if { [catch {
-  file copy -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom.dcp C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom.dcp
+  file copy -force G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom.dcp G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.v
+  write_verilog -force -mode synth_stub G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.vhdl
+  write_vhdl -force -mode synth_stub G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
+  write_verilog -force -mode funcsim G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -120,46 +123,46 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom.dcp C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom.dcp
+  file copy -force G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom.dcp G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_stub.v C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.v
+  file rename -force G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_stub.v G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_stub.vhdl C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.vhdl
+  file rename -force G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_stub.vhdl G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_sim_netlist.v C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
+  file rename -force G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_sim_netlist.v G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_sim_netlist.vhdl C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.vhdl
+  file rename -force G:/Study2024S/CS202-SpringProject/Project/Lab9.runs/prgrom_synth_1/prgrom_sim_netlist.vhdl G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 }; # end if cached_ip 
 
-if {[file isdir C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom]} {
+if {[file isdir G:/Study2024S/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom]} {
   catch { 
-    file copy -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.v C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom
+    file copy -force G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.v G:/Study2024S/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom
   }
 }
 
-if {[file isdir C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom]} {
+if {[file isdir G:/Study2024S/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom]} {
   catch { 
-    file copy -force C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.vhdl C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom
+    file copy -force G:/Study2024S/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_stub.vhdl G:/Study2024S/CS202-SpringProject/Project/Lab9.ip_user_files/ip/prgrom
   }
 }
--- Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
+++ Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
@@ -3,8 +3,8 @@
 // Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
 // Date        : Thu May  9 14:48:41 2024
 // Host        : Sigma running 64-bit major release  (build 9200)
-// Command     : write_verilog -force -mode funcsim
-//               C:/Users/86153/Desktop/cpu2/CS202-SpringProject/Project/Lab9.srcs/sources_1/ip/prgrom/prgrom_sim_netlist.v
+// Command     : write_verilog -force -mode funcsim -rename_top prgrom -prefix
+//               prgrom_ prgrom_sim_netlist.v
 // Design      : prgrom
 // Purpose     : This verilog netlist is a functional simulation representation of the design and should not be modified
 //               or synthesized. This netlist cannot be used for SDF annotated simulation.
@@ -192,7 +192,6 @@ module prgrom
         .web(1'b0));
 endmodule
 
-(* ORIG_REF_NAME = "bindec" *) 
 module prgrom_bindec
    (ena_array,
     addra);
@@ -216,7 +215,6 @@ module prgrom_bindec
         .O(ena_array[1]));
 endmodule
 
-(* ORIG_REF_NAME = "blk_mem_gen_generic_cstr" *) 
 module prgrom_blk_mem_gen_generic_cstr
    (douta,
     addra,
@@ -493,7 +491,6 @@ module prgrom_blk_mem_gen_generic_cstr
         .wea(wea));
 endmodule
 
-(* ORIG_REF_NAME = "blk_mem_gen_mux" *) 
 module prgrom_blk_mem_gen_mux
    (douta,
     addra,
@@ -867,7 +864,6 @@ module prgrom_blk_mem_gen_mux
         .R(1'b0));
 endmodule
 
-(* ORIG_REF_NAME = "blk_mem_gen_prim_width" *) 
 module prgrom_blk_mem_gen_prim_width
    (douta,
     clka,
@@ -1368,7 +1364,6 @@ module prgrom_blk_mem_gen_prim_width__parameterized9
         .wea(wea));
 endmodule
 
-(* ORIG_REF_NAME = "blk_mem_gen_prim_wrapper_init" *) 
 module prgrom_blk_mem_gen_prim_wrapper_init
    (douta,
     clka,
@@ -4975,7 +4970,6 @@ module prgrom_blk_mem_gen_prim_wrapper_init__parameterized9
         .WEBWE({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}));
 endmodule
 
-(* ORIG_REF_NAME = "blk_mem_gen_top" *) 
 module prgrom_blk_mem_gen_top
    (douta,
     addra,
@@ -5026,7 +5020,7 @@ endmodule
 (* C_USE_URAM = "0" *) (* C_WEA_WIDTH = "1" *) (* C_WEB_WIDTH = "1" *) 
 (* C_WRITE_DEPTH_A = "16384" *) (* C_WRITE_DEPTH_B = "16384" *) (* C_WRITE_MODE_A = "WRITE_FIRST" *) 
 (* C_WRITE_MODE_B = "WRITE_FIRST" *) (* C_WRITE_WIDTH_A = "32" *) (* C_WRITE_WIDTH_B = "32" *) 
-(* C_XDEVICEFAMILY = "artix7" *) (* ORIG_REF_NAME = "blk_mem_gen_v8_4_1" *) (* downgradeipidentifiedwarnings = "yes" *) 
+(* C_XDEVICEFAMILY = "artix7" *) (* downgradeipidentifiedwarnings = "yes" *) 
 module prgrom_blk_mem_gen_v8_4_1
    (clka,
     rsta,
@@ -5288,7 +5282,6 @@ module prgrom_blk_mem_gen_v8_4_1
         .wea(wea));
 endmodule
 
-(* ORIG_REF_NAME = "blk_mem_gen_v8_4_1_synth" *) 
 module prgrom_blk_mem_gen_v8_4_1_synth
    (douta,
     addra,
