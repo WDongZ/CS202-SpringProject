de4bba65f00a99498d4b74dfabf07fd9120f3fd9
mapleZhu <295909576@qq.com>
2024-05-13 22:52:53 +0800

    update jal

--- Project/Lab9.srcs/sources_1/new/IFetch.v
+++ Project/Lab9.srcs/sources_1/new/IFetch.v
@@ -1,13 +1,13 @@
 module IFetch(
-    clk,branch,zero,imm,bne,PC_out
+    clk,branch,zero,jal,imm,PC_out
     );
-    input clk,branch,zero,bne;
+    input clk,branch,zero,jal;
     input [31:0] imm;
     output [31:0] PC_out;
     reg [31:0] PC;
     initial PC <= 0;
     always @ (posedge clk) begin
-        if(branch & zero) PC <= PC + {imm,1'b0};
+        if((branch && zero)||jal) PC <= PC + {imm,1'b0};
         else PC <= PC + 4;
     end
     assign PC_out = PC;
