8d8b906444a235dbcc5916dd236c967e65b50616
WDongZ <505314385@qq.com>
2024-05-14 16:36:03 +0800

    ud

---
+++ Project/Lab9.srcs/sim_1/new/tb_CPU.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/05/14 15:38:22
+// Design Name: 
+// Module Name: tb_CPU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_CPU(
+
+    );
+endmodule
